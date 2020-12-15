39da978eddeafba45726601b5ac34d64be72b488
enginepdf <yeindap@gmail.com>
2020-12-15 23:25:06 +0900

    FIX some errors'

--- server/scrapingItems.js
+++ server/scrapingItems.js
@@ -8,12 +8,28 @@
 
 //  npx sequelize-cli db:migrate
 
+
+
+
 const { items } = require('./models');
 
+const Xvfb=require('xvfb');
 const cheerio=require("cheerio");
 const Nightmare=require("nightmare");
 const nightmare=Nightmare({show:false});  // show browser
 const fs=require('fs');
+
+
+// apt-get update
+// apt-get install -y xvfb x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps clang libdbus-1-dev libgtk2.0-dev libnotify-dev libgnome-keyring-dev libgconf2-dev libasound2-dev libcap-dev libcups2-dev libxtst-dev libxss1 libnss3-dev gcc-multilib g++-multilib
+
+let xvfb=new Xvfb(); // for nightmare.js on remote linux server
+
+try {
+   xvfb.startSync();
+} catch(e){
+   console.log(e);
+}
 
 const sampleResult={
     title:"THOM BROWNE",
@@ -96,7 +112,7 @@ async function itemsScraper(){
    // let data={};
       let data=[];
    // for(const [key] of Object.entries(category)){
-      for(let i=1 ; i<6 ; i++){
+      for(let i=1 ; i<=1 ; i++){
          let key='NewIn' // new in
          let scraped=await scrape(i,key);
          data=[...data, ...scraped];
