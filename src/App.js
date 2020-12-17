4fcf95d286217ce60938c9b9e446e4b6a3c4d66a
enginepdf <yeindap@gmail.com>
2020-12-17 12:45:01 +0900

    ADD final client

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import './App.css';
+
+// import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLogin: false,
+      items:[]
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://18.222.147.153:3050/items')
+      .then(res => res.json())
+      .then((data) => {
+        let item=JSON.parse(data)
+        this.setState({ items: item });
+      });
+  }
+
+  toggleLogin() {
+    if(this.state.isLogin){
+      fetch('http://18.222.147.153:3050/signout')
+        .then(res=>res.json())
+        .then((data) => {
+          console.log('logged out');
+        });
+    } 
+    this.setState(prevState => ({ isLogin: !prevState.isLogin }));
+  }
+  render(){
+    return (
+     <div>
+        <Header isLogin={this.state.isLogin} toggleLogin={this.toggleLogin.bind(this)}/>
+        <Switch>
+          <Route exact path='/' render={()=><ShopPage items={this.state.items} />} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.state.isLogin ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage toggleLogin={this.toggleLogin.bind(this)} />
+              )
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom'
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();   // stopping the original function of form or a, submit tags
+
+    fetch("http://18.222.147.153:3050/signin", {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.state)
+              })
+              // .then(res => res.json())
+              .then(res=>{
+                if(res.status === 200) {
+                  this.props.toggleLogin();
+                  this.props.history.push('/')
+              }})
+              .catch(err => console.log(err));
+
+    this.setState({ email: '', password: '' });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });   // name : email or password
+  };
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <CustomButton type='submit'> Sign in </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withRouter(SignIn);
